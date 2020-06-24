4742673b99dccc0475a9d99ac585bd6ca942bf06
Sayan Ghosh <sgic2001@gmail.com>
2020-06-24 16:44:50 +0530

    Add contact page

---
+++ contact/js/scripts.js
@@ -0,0 +1,24 @@
+function SendMail(){
+	var link = 'mailto:sgic2001@gmail.com';
+	var subject,body,i=0;
+	Subject = document.getElementById("subject");
+	Body = document.getElementById("body");
+	subject = Subject.value;
+	body	= Body.value;
+	subject = encodeURIComponent(subject);
+	body = encodeURIComponent(body);
+	if( subject!='' ) {
+		link += (i++==0?'?':'&');
+		link += 'subject='+subject;
+	}
+	if( body!='' ) {
+		link += (i++==0?'?':'&');
+		link += 'body='+body;
+	}
+	link=link.replace(/%0A/g,"%0D%0A");
+	window.open(link);
+	Subject.value="";
+	Body.value="";
+}
+Button = document.getElementById("button");
+Button.addEventListener("click", SendMail);
