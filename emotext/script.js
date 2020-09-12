ff54024d59e642293b0b3e6ecf95f7e27f36b51d
Sayan Ghosh <sgic2001@gmail.com>
2020-09-12 11:25:21 +0530

    Add copy to clipboard button in emotext

--- emotext/script.js
+++ emotext/script.js
@@ -1,12 +1,17 @@
-input = document.querySelector("#input");
-output = document.querySelector("#output");
-button = document.querySelector("#submit");
+var input = document.querySelector("#input");
+var output = document.querySelector("#output");
+var sub = document.querySelector("#submit");
+var copy = document.querySelector("#copy");
+var out = ""
 
-button.addEventListener("click", submit);
+sub.addEventListener("click", submit);
 input.addEventListener("keyup",(e)=>{
     if(e.keyCode === 13){
         submit();
     }
+});
+copy.addEventListener("click", ()=>{
+    copyTextToClipboard(out);
 });
 
 function submit(){
@@ -16,23 +21,25 @@ function submit(){
         "🇷","🇸","🇹","🇺","🇻","🇼","🇽","🇾", "🇿"];
     number = ["0️⃣","1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣",
               "9️⃣"]
-    output.innerHTML = "";
+    out = "";
     var len = text.length;
     for (var i = 0; i < len; i++){
         var char = text.charAt(i);
         if(isAlpha(char)){
             var char2 = letter[char.charCodeAt(0)-65];
-            output.innerHTML += char2 + " ";
+            out += char2 + " ";
         }
         else if(isNum(char)){
             var char2 = number[char.charCodeAt(0)-48];
-            output.innerHTML += char2 + "";
+            out += char2 + "";
         }
         else{
-            output.innerHTML += char;
+            out += char + "";
             continue;
         }
     }
+    output.innerHTML = out;
+    copy.disabled = out==="";
 }
 function isAlpha(str) {
     return /^[a-zA-Z]+$/.test(str);
@@ -40,3 +47,37 @@ function isAlpha(str) {
 function isNum(str) {
     return /^[0-9]+$/.test(str);
 }
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
