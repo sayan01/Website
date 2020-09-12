b7f0f21ee2fc6d37c6dbb2654e329197932f0162
Sayan Ghosh <sgic2001@gmail.com>
2020-09-12 10:13:20 +0530

    Fix style and add numbers in emotext

--- emotext/script.js
+++ emotext/script.js
@@ -1,23 +1,42 @@
-document.querySelector("#submit").addEventListener("click", ()=>{
-    text = document.querySelector("#input").value;
-    output = document.querySelector("#output");
-    array = ["🇦","🇧","🇨","🇩","🇪", "🇫", "🇬",
-             "🇭", "🇮", "🇯", "🇰", "🇱", "🇲", "🇳",
-             "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺",
-             "🇻", "🇼", "🇽", "🇾", "🇿"];
-    output.innerHTML = "";
-    var len = text.length;
-    for (var i = 0; i < len; i++){
-        var char = text.charAt(i).toUpperCase();
-        if(!isAlpha(char)){
-            output.innerHTML = output.innerHTML + char;
-            continue;
-        }
-        var char2 = array[char.charCodeAt(0)-65];
-        output.innerHTML = output.innerHTML + char2 + " ";
+input = document.querySelector("#input");
+output = document.querySelector("#output");
+button = document.querySelector("#submit");
+
+button.addEventListener("click", submit);
+input.addEventListener("keyup",(e)=>{
+    if(e.keyCode === 13){
+        submit();
     }
 });
 
+function submit(){
+    text = input.value.toUpperCase();
+    letter = ["🇦","🇧","🇨","🇩","🇪","🇫","🇬","🇭",
+        "🇮","🇯","🇰","🇱","🇲","🇳","🇴","🇵","🇶",
+        "🇷","🇸","🇹","🇺","🇻","🇼","🇽","🇾", "🇿"];
+    number = ["0️⃣","1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣",
+              "9️⃣"]
+    output.innerHTML = "";
+    var len = text.length;
+    for (var i = 0; i < len; i++){
+        var char = text.charAt(i);
+        if(isAlpha(char)){
+            var char2 = letter[char.charCodeAt(0)-65];
+            output.innerHTML += char2 + " ";
+        }
+        else if(isNum(char)){
+            var char2 = number[char.charCodeAt(0)-48];
+            output.innerHTML += char2 + "";
+        }
+        else{
+            output.innerHTML += char;
+            continue;
+        }
+    }
+}
 function isAlpha(str) {
     return /^[a-zA-Z]+$/.test(str);
-  }
+}
+function isNum(str) {
+    return /^[0-9]+$/.test(str);
+}
